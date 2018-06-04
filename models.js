4ac6adf0603fc727e130c211d955bc1eacd35bde
Horacio Gutierrez <hogutier@yahoo.com>
2018-06-04 11:28:19 -0500

    completed seed and instance methods

--- models.js
+++ models.js
@@ -2,12 +2,6 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('postgres://localhost:5432/plantr', {logging: true});
 
-
-db.authenticate()
-  .then( () => console.log('connected to the database')   )
-
-  module.exports = db;
-
 const Gardener = db.define('gardener', {
   name: {type: Sequelize.STRING},
   age: {type: Sequelize.INTEGER},
@@ -26,19 +20,11 @@ const Vegetable = db.define('vegetable', {
 
 Plot.belongsTo(Gardener);
 Gardener.hasOne(Plot);
+//The "through" parameter defines the name of the join table that gets created.
 Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
 Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
-//The "through" parameter defines the name of the join table that gets created.
 
+//the "as" parameter creates an alias for vegetable
 Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
 
-Vegetable.bulkCreate([
-  {name: 'spinach', color: 'green'},
-  {name: 'carrot', color: 'red'},
-  {name: 'beet', color: 'purple'},
-  {name: 'cauliflower', color: 'white'}
-])
-.then(() => {
-  return Plot.create({size: 100, shaded: true})
-})
-.catch(err => console.log(err))
+module.exports = { db, Gardener, Plot, Vegetable }
--- seed.js
+++ seed.js
@@ -1,12 +1,59 @@
-const db = require('./models');
+const { db, Gardener, Plot, Vegetable } = require("./models");
 
-db.sync({force: true})
-  .then (() => {
-    console.log('Database synced!');
-    db.close();
-  })
+const seed = async () => {
+  await db.sync({force: true})
+  console.log('database synced!')
+
+  const vegetables = await Promise.all([
+    Vegetable.create({ name: "spinach", color: "green" }),
+    Vegetable.create({ name: "carrot", color: "red" }),
+    Vegetable.create({ name: "beet", color: "purple" }),
+    Vegetable.create({ name: "cauliflower", color: "white" })
+  ])
+  console.log(`seeded ${vegetables.length} vegetables successfully`)
+  const spinach = vegetables[0]
+  const carrot = vegetables[1]
+  const beet = vegetables[2]
+  const cauliflower = vegetables[3]
+
+  const gardeners = await Promise.all([
+    Gardener.create({name: 'John Smith', age: 22}),
+    Gardener.create({name: 'Jim Clark', age: 40})
+  ])
+
+  const johnSmith = gardeners[0]
+  const jimClark = gardeners[1]
+
+  console.log(`seeded ${gardeners.length} gardener(s) successfully`)
+
+  const plots = await Promise.all([
+    Plot.create({ size: 100, shaded: true, gardenerId: johnSmith.id}),
+    Plot.create({ size: 200, shaded: false}),
+    Plot.create({ size: 200, shaded: false})
+  ])
+  console.log(`seeded ${plots.length} plot(s) successfully`)
+  const plot1 = plots[0]
+  const plot2 = plots[1]
+  const plot3 = plots[2]
+
+  plot2.setGardener(johnSmith.id)
+  plot3.setGardener(jimClark.id)
+
+  const PlotVegetable = db.model('vegetable_plot')
+  return PlotVegetable.create({vegetableId: carrot.id, plotId: plot1.id})
+
+}
+
+seed()
   .catch(err => {
-    console.log("Something went wrong");
-    console.log(err);
-    db.close();
+    console.error(err.message)
+    console.error(err.stack)
+    process.exit(1)
   })
+  .then(() => {
+    console.log('closing db connection')
+    db.close()
+    console.log('db connection closed')
+  })
+
+console.log('seeding...')
